31fb236c9d09f6fed2d26334b03e099c6c76143d
Thomas Dorr <tdorr@sonatype.com>
2019-05-24 03:48:10 -0400

    Updated tgenv version
    Updated tgenv version

--- Formula/tgenv.rb
+++ Formula/tgenv.rb
@@ -1,8 +1,8 @@
 class Tgenv < Formula
   desc "Terragrunt version manager inspired by tfenv"
   homepage "https://github.com/sigsegv13/tgenv"
-  url "https://github.com/sigsegv13/tgenv/archive/v0.0.4.tar.gz"
-  sha256 "25c1b9784845c744beb5945406abbf00e17922bb4ca39e7052650c4601531f6a"
+  url "https://github.com/sigsegv13/tgenv/archive/v0.0.5.tar.gz"
+  sha256 "8998d5707410ac3cd5fcb3b972e79a78fdc21694dfce39b2f2bd85971fd51b02"
   head "https://github.com/sigsegv13/tgenv.git"
 
   bottle :unneeded
