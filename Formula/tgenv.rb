a45a3a7d722aedea3e115231665c2014c61b5e2a
Thomas Dorr <tdorr@sonatype.com>
2019-05-24 01:13:13 -0400

    Updated version in tgenv formula
    Forgot to update version number in prior commit of formula. (D'oh!)

--- Formula/tgenv.rb
+++ Formula/tgenv.rb
@@ -1,7 +1,7 @@
 class Tgenv < Formula
   desc "Terragrunt version manager inspired by tfenv"
   homepage "https://github.com/sigsegv13/tgenv"
-  url "https://github.com/sigsegv13/tgenv/archive/v0.0.2.tar.gz"
+  url "https://github.com/sigsegv13/tgenv/archive/v0.0.3.tar.gz"
   sha256 "0273a0e2f7433b646d5eddef4eb3ee8b9d5b011b4aba62752093192692966d93"
   head "https://github.com/sigsegv13/tgenv.git"
 
